afca548c38257bae3aff22e514874a124ba9ee14
Chaos Adept <38735292+e3Dr8nj@users.noreply.github.com>
2020-06-18 23:51:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,16 @@
-const Discord = require('discord.js');
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+/*const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
@@ -27,3 +39,4 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.DISCORD_TOKEN);//DISCORD_TOKEN is the Client Secret
+*/
